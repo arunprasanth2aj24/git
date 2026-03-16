4efb0cc90ace4f8460372b4074ef901af5757bf9
arunprasanth2aj24 <arunprasanth2aj24@kprcas.ac.in>
2026-03-16 23:30:45 +0530

    mew branch

---
+++ src/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+
+    public static void new1 (){
+        System.out.println("New program");
+    }
+
+    public static void new2(){
+        System.out.println("Changes of new branch");
+    }
+}
